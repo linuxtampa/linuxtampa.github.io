4d433a41d4287d93881b25c68d5bf534dac9cc85
Tim Bailey-Jones <tim@bailey-jones.com>
2026-03-17 16:30:22 -0400

    add resume

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -x -e -u -o pipefail
-cp ~/symlink-resume-2025/Tim-Bailey-Jones-Resume-*.pdf _site
 npm run build
 gcloud storage rsync _site/. gs://linuxtampa.com --recursive
 gcloud compute url-maps invalidate-cdn-cache website-url-map --global --path=/*  # --async 
